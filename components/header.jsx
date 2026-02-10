c02b7f4fa8f0f45bb5f91877322ead85623750e0
Rohit Jangra <jangra8295829804@gmail.com>
2026-02-10 22:07:48 +0530

    detailed landing page

--- components/header.jsx
+++ components/header.jsx
@@ -1,12 +1,23 @@
+"use client";
+
+import { SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "./ui/button";
+import { Authenticated, Unauthenticated } from "convex/react";
+import { BarLoader } from "react-spinners";
+import { useStoreUser } from "../hooks/use-store-user";
+import { Plus, Building, Ticket } from "lucide-react";
 
 const Header = () => {
+  const { isLoading } = useStoreUser();
+
+  const [showUpgradeModal, setShowUpgradeModal] = useState(false);
   return (
     <>
       <nav className="fixed top-0 left-0 right-0 bg-background/80 backdrop-blur-xl z-20 border-b">
-        <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between ">
+        <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
           {/* Logo */}
           <Link href={"/"} className="flex items-center">
             <Image
@@ -17,17 +28,68 @@ const Header = () => {
               className="w-full h-11"
               priority
             />
-
-            {/* Pro Badge */}
           </Link>
 
-          {/* Search and Location - Desktop only  */}
-
           {/* Right side Actions */}
+          <div className="flex items-center">
+            <Button
+              variant={"ghost"}
+              size="sm"
+              onClick={() => setShowUpgradeModal(true)}
+            >
+              Pricing
+            </Button>
+
+            <Button variant="ghost" size="sm" asChild className={"mr-2"}>
+              <Link href="/explore">Explore</Link>
+            </Button>
+
+            <Authenticated>
+              <Button size="sm" asChild className="flex gap-2 mr-4">
+                <Link href="/create-event">
+                  <Plus className="w-4 h-4" />
+                  <span className="hidden sm:inline">Create Event</span>
+                </Link>
+              </Button>
+
+              <UserButton>
+                <UserButton.MenuItems>
+                  <UserButton.Link
+                    label="My Tickets"
+                    labelIcon={<Ticket size={16} />}
+                    href="/my-tickets"
+                  />
+
+                  <UserButton.Link
+                    label="My Events"
+                    labelIcon={<Building size={16} />}
+                    href="/my-events"
+                  />
+                </UserButton.MenuItems>
+                <UserButton.MenuItems>
+                  <UserButton.Action label="manageAccount" />
+                </UserButton.MenuItems>
+              </UserButton>
+            </Authenticated>
+
+            <Unauthenticated>
+              <SignInButton mode="modal">
+                <Button size="sm">Sign In</Button>
+              </SignInButton>
+            </Unauthenticated>
+            {/* Show the user button when the user is signed in */}
+          </div>
         </div>
-        {/* Mobile Search and Location - Below Header */}
+
+        {/* Mobile Search and Location Below Header */}
+
+        {/* Loaders */}
+        {isLoading && (
+          <div className="absolute bottom-0 left-0 w-full">
+            <BarLoader width={"100%"} color="#a855f7" />
+          </div>
+        )}
       </nav>
-      {/* Modals */}
     </>
   );
 };
